9aadf304c4dd626ad201a5aa71606da66dfd2e07
sharmin <sharminshantisn90@gmail.com>
2024-11-17 11:19:07 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let button=document.querySelector(".pin")
+let button2=document.querySelector(".yell")
+let button3=document.querySelector(".gree")
+let button4=document.querySelector(".pur")
+let button5=document.querySelector(".blac")
+let button6=document.querySelector(".blu")
+let button7=document.querySelector(".re")
+let heading=document.querySelector(".one")
+let heading2=document.querySelector("h1")
+let button8=document.querySelector(".res")
+
+
+button.addEventListener("click",function(){
+   heading.style.background="pink"
+   heading2.style.color="teal"
+
+})
+button2.addEventListener("click",function(){
+    heading.style.background="yellow"
+    heading2.style.color="purple"
+ 
+ })
+ button3.addEventListener("click",function(){
+    heading.style.background="green"
+    heading2.style.color="black"
+ 
+ })
+ button4.addEventListener("click",function(){
+    heading.style.background="purple"
+    heading2.style.color="yellow"
+ 
+ })
+ button5.addEventListener("click",function(){
+    heading.style.background="black"
+    heading2.style.color="white"
+ 
+ })
+ button6.addEventListener("click",function(){
+    heading.style.background="blue"
+    heading2.style.color="orange"
+ 
+ })
+ button7.addEventListener("click",function(){
+    heading.style.background="red"
+    heading2.style.color="green"
+ 
+ })
+ button8.addEventListener("click",function(){
+    heading.style.background="tomato"
+    heading2.style.color="blue"
+ 
+ })
